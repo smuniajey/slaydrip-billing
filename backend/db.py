4da83e68ec1202e30ea32260aab4ece7dc3f26f9
smuniajey <smuniajey@gmail.com>
2026-01-04 03:04:00 +0530

    Fix imports and DATABASE_URL usage for Render

--- backend/db.py
+++ backend/db.py
@@ -1,11 +1,8 @@
-import os
 import psycopg2
-from psycopg2.extras import RealDictCursor
+import os
 
 def get_connection():
     return psycopg2.connect(
         os.environ["postgresql://neondb_owner:npg_aGzdo2jblwx9@ep-holy-smoke-a4e3gdau-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"],
-        cursor_factory=RealDictCursor,
         sslmode="require"
     )
-
