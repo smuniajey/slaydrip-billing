a19a4d8f3ff146dc57d6fa1696747186048ec8ef
smuniajey <smuniajey@gmail.com>
2026-01-04 03:16:10 +0530

    Fix DATABASE_URL env usage for Neon

--- backend/db.py
+++ backend/db.py
@@ -3,6 +3,5 @@ import os
 
 def get_connection():
     return psycopg2.connect(
-        os.environ["postgresql://neondb_owner:npg_aGzdo2jblwx9@ep-holy-smoke-a4e3gdau-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"],
-        sslmode="require"
+        os.environ["postgresql://neondb_owner:npg_aGzdo2jblwx9@ep-holy-smoke-a4e3gdau-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"]
     )
